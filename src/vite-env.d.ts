5142dd940446da3808c5e5d93d9fd74c08518c7f
AlejandroDeveloper2 <90485793+AlejandroDeveloper2@users.noreply.github.com>
2024-02-16 15:02:16 -0500

    Cambios

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_QUIZ_ID_PRODUCTION: string;
+  readonly VITE_POCKETBASE_URL_PRODUCTION: string;
+  readonly VITE_QUIZ_ID_LOCAL: string;
+  readonly VITE_POCKETBASE_URL_LOCAL: string;
+  // more env variables...
+}
